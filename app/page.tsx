691cd5d853cdcba4f4684950ac7141219aa1b8a9
Matthew <wallaceomatthew@gmail.com>
2025-09-29 13:44:16 -0500

    auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div>
       <main>
-        <h1>Good Day</h1>
+        <h1>Hello Day</h1>
+        <Button> Click Me </Button>
       </main>
     </div>
   );
